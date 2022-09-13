469cf60c6836a2482bc320548993896ba8047742
tomholford <tomholford@users.noreply.github.com>
2022-09-13 14:53:39 -0700

    meta: rename Homestead --> Groups
    This updates the hardcoded references from %homstead to %groups per https://github.com/tloncorp/homestead/pull/861

--- sync.sh
+++ sync.sh
@@ -2,12 +2,12 @@
 
 SHIP="${1:-zod}"
 URBIT_PATH=~/dev/urbit/urbit
-HOMESTEAD_PATH=~/dev/urbit/homestead
+REPO_PATH=~/dev/urbit/homestead
 SHIP_PATH=~/dev/urbit/ships
 
 rsync -avqL --delete $URBIT_PATH/pkg/base-dev/* $SHIP_PATH/$SHIP/garden/ && \
 rsync -avqL $URBIT_PATH/pkg/garden/* $SHIP_PATH/$SHIP/garden/ && \
-rsync -avqL --delete $URBIT_PATH/pkg/base-dev/* $SHIP_PATH/$SHIP/homestead/ && \
-rsync -avqL $URBIT_PATH/pkg/garden-dev/* $SHIP_PATH/$SHIP/homestead/ && \
-rsync -avqL $HOMESTEAD_PATH/desk/* $SHIP_PATH/$SHIP/homestead/ && \
-rsync -avqL $HOMESTEAD_PATH/landscape-dev/* $SHIP_PATH/$SHIP/homestead/
+rsync -avqL --delete $URBIT_PATH/pkg/base-dev/* $SHIP_PATH/$SHIP/groups/ && \
+rsync -avqL $URBIT_PATH/pkg/garden-dev/* $SHIP_PATH/$SHIP/groups/ && \
+rsync -avqL $REPO_PATH/desk/* $SHIP_PATH/$SHIP/groups/ && \
+rsync -avqL $REPO_PATH/landscape-dev/* $SHIP_PATH/$SHIP/groups/
